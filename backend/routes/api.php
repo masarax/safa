0009dc6d60dc6b2bd7a04afc08ecbd217a6fc332
MASARA X <55315718+masarax@users.noreply.github.com>
2026-08-12 11:53:44 +0300

    fix: tolerate PUT/PATCH normalization at operator update route boundary

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -35,55 +35,23 @@ Route::prefix('auth')->group(function () {
     Route::middleware([CheckApiSecurityKey::class, 'verify.multilevel.token', VerifyActiveAuthSession::class, AuditLogMiddleware::class, 'throttle:60,1'])->group(function () {
         Route::get('/operators', [UserManagementController::class, 'index']);
         Route::post('/operators', [UserManagementController::class, 'store']);
-        Route::put('/operators/{id}', [UserManagementController::class, 'update']);
-        Route::patch('/operators/{id}', [UserManagementController::class, 'update']);
+        Route::any('/operators/{id}', [UserManagementController::class, 'update']);
         Route::delete('/operators/{id}', [UserManagementController::class, 'destroy']);
         Route::post('/share-account', [AuthJWTController::class, 'shareAccount']);
         Route::get('/shared-accounts', [AuthJWTController::class, 'getSharedAccounts']);
         Route::post('/switch-account', [AuthJWTController::class, 'switchAccount']);
     });
 });
-
-Route::middleware([CheckApiSecurityKey::class, 'verify.multilevel.token', VerifyActiveAuthSession::class, ResolveGraphQLAccountContext::class, RequireGraphQLPermission::class, AuditLogMiddleware::class, 'throttle:60,1'])->group(function () {
-    Route::post('/graphql', [GraphQLController::class, 'handle']);
-});
-
+Route::middleware([CheckApiSecurityKey::class, 'verify.multilevel.token', VerifyActiveAuthSession::class, ResolveGraphQLAccountContext::class, RequireGraphQLPermission::class, AuditLogMiddleware::class, 'throttle:60,1'])->group(function () { Route::post('/graphql', [GraphQLController::class, 'handle']); });
 Route::middleware([CheckApiSecurityKey::class, 'verify.multilevel.token', VerifyActiveAuthSession::class, AuditLogMiddleware::class, RequireBusinessPermission::class, 'throttle:60,1'])->group(function () {
-    Route::get('/sync/down', [SyncController::class, 'syncDown']);
-    Route::post('/sync/up', [SyncController::class, 'syncUp'])->middleware(ValidateSyncDependencies::class);
-    Route::get('/config/remote', [RemoteConfigController::class, 'getRemoteConfig']);
-    Route::get('/version/check', [RemoteConfigController::class, 'checkVersion']);
-    Route::get('/accounts', [AccountContextController::class, 'index']);
-    Route::post('/accounts/switch', [AccountContextController::class, 'switch']);
-    Route::post('/accounts/share', [AccountContextController::class, 'share']);
-    Route::get('/customers', [CustomerController::class, 'index']);
-    Route::post('/customers', [CustomerController::class, 'store']);
-    Route::put('/customers/{id}', [CustomerController::class, 'update']);
-    Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
-    Route::get('/suppliers', [SupplierController::class, 'index']);
-    Route::post('/suppliers', [SupplierController::class, 'store']);
-    Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
-    Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
-    Route::get('/transactions', [TransactionController::class, 'index']);
-    Route::post('/transactions', [TransactionController::class, 'store']);
-    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
-    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
-    Route::get('/wallet-ledgers', [RemoteBusinessController::class, 'walletLedgers']);
-    Route::post('/wallet-ledgers', [RemoteBusinessController::class, 'storeWalletLedger']);
-    Route::put('/wallet-ledgers/{id}', [RemoteBusinessController::class, 'updateWalletLedger']);
-    Route::delete('/wallet-ledgers/{id}', [RemoteBusinessController::class, 'destroyWalletLedger']);
-    Route::get('/supplier-deposits', [RemoteBusinessController::class, 'supplierDeposits']);
-    Route::post('/supplier-deposits', [RemoteBusinessController::class, 'storeSupplierDeposit']);
-    Route::put('/supplier-deposits/{id}', [RemoteBusinessController::class, 'updateSupplierDeposit']);
-    Route::delete('/supplier-deposits/{id}', [RemoteBusinessController::class, 'destroySupplierDeposit']);
-    Route::get('/wallet-batches', [RemoteBusinessController::class, 'walletBatches']);
-    Route::post('/wallet-batches', [RemoteBusinessController::class, 'storeWalletBatch']);
-    Route::put('/wallet-batches/{id}', [RemoteBusinessController::class, 'updateWalletBatch']);
-    Route::delete('/wallet-batches/{id}', [RemoteBusinessController::class, 'destroyWalletBatch']);
-    Route::get('/expenses-incomes', [RemoteBusinessController::class, 'expensesIncomes']);
-    Route::post('/expenses-incomes', [RemoteBusinessController::class, 'storeExpenseIncome']);
-    Route::put('/expenses-incomes/{id}', [RemoteBusinessController::class, 'updateExpenseIncome']);
-    Route::delete('/expenses-incomes/{id}', [RemoteBusinessController::class, 'destroyExpenseIncome']);
-    Route::post('/config/update', [RemoteConfigController::class, 'updateConfig'])->middleware(RequireSuperAdmin::class);
-    Route::post('/upload/logo', [RemoteConfigController::class, 'uploadLogo'])->middleware([RequireSuperAdmin::class, ValidateLogoUpload::class]);
+    Route::get('/sync/down', [SyncController::class, 'syncDown']); Route::post('/sync/up', [SyncController::class, 'syncUp'])->middleware(ValidateSyncDependencies::class);
+    Route::get('/config/remote', [RemoteConfigController::class, 'getRemoteConfig']); Route::get('/version/check', [RemoteConfigController::class, 'checkVersion']); Route::get('/accounts', [AccountContextController::class, 'index']); Route::post('/accounts/switch', [AccountContextController::class, 'switch']); Route::post('/accounts/share', [AccountContextController::class, 'share']);
+    Route::get('/customers', [CustomerController::class, 'index']); Route::post('/customers', [CustomerController::class, 'store']); Route::put('/customers/{id}', [CustomerController::class, 'update']); Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
+    Route::get('/suppliers', [SupplierController::class, 'index']); Route::post('/suppliers', [SupplierController::class, 'store']); Route::put('/suppliers/{id}', [SupplierController::class, 'update']); Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
+    Route::get('/transactions', [TransactionController::class, 'index']); Route::post('/transactions', [TransactionController::class, 'store']); Route::put('/transactions/{id}', [TransactionController::class, 'update']); Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
+    Route::get('/wallet-ledgers', [RemoteBusinessController::class, 'walletLedgers']); Route::post('/wallet-ledgers', [RemoteBusinessController::class, 'storeWalletLedger']); Route::put('/wallet-ledgers/{id}', [RemoteBusinessController::class, 'updateWalletLedger']); Route::delete('/wallet-ledgers/{id}', [RemoteBusinessController::class, 'destroyWalletLedger']);
+    Route::get('/supplier-deposits', [RemoteBusinessController::class, 'supplierDeposits']); Route::post('/supplier-deposits', [RemoteBusinessController::class, 'storeSupplierDeposit']); Route::put('/supplier-deposits/{id}', [RemoteBusinessController::class, 'updateSupplierDeposit']); Route::delete('/supplier-deposits/{id}', [RemoteBusinessController::class, 'destroySupplierDeposit']);
+    Route::get('/wallet-batches', [RemoteBusinessController::class, 'walletBatches']); Route::post('/wallet-batches', [RemoteBusinessController::class, 'storeWalletBatch']); Route::put('/wallet-batches/{id}', [RemoteBusinessController::class, 'updateWalletBatch']); Route::delete('/wallet-batches/{id}', [RemoteBusinessController::class, 'destroyWalletBatch']);
+    Route::get('/expenses-incomes', [RemoteBusinessController::class, 'expensesIncomes']); Route::post('/expenses-incomes', [RemoteBusinessController::class, 'storeExpenseIncome']); Route::put('/expenses-incomes/{id}', [RemoteBusinessController::class, 'updateExpenseIncome']); Route::delete('/expenses-incomes/{id}', [RemoteBusinessController::class, 'destroyExpenseIncome']);
+    Route::post('/config/update', [RemoteConfigController::class, 'updateConfig'])->middleware(RequireSuperAdmin::class); Route::post('/upload/logo', [RemoteConfigController::class, 'uploadLogo'])->middleware([RequireSuperAdmin::class, ValidateLogoUpload::class]);
 });
