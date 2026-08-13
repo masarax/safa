d6b4c2a384a648d258714807192e7b417e56b31a
MASARA X <55315718+masarax@users.noreply.github.com>
2026-08-13 15:36:54 +0300

    chore(auth): remove unused migration command import

--- backend/app/Console/Commands/MigrateLegacyOperatorAccounts.php
+++ backend/app/Console/Commands/MigrateLegacyOperatorAccounts.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Support\MobileNumber;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class MigrateLegacyOperatorAccounts extends Command
 {
