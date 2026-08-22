4c1b87de33ac10a96860bc87b45b0b8aeff4faee
MASARA X <55315718+masarax@users.noreply.github.com>
2026-08-22 09:26:16 +0300

    fix: apply secure-window policy to all activities

--- app/src/main/java/com/safa/account/SafaApplication.kt
+++ app/src/main/java/com/safa/account/SafaApplication.kt
@@ -1,11 +1,26 @@
 package com.safa.account
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.safa.account.data.sync.SyncWorkScheduler
+import com.safa.account.ui.security.SensitiveWindowPolicy
 
 class SafaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                SensitiveWindowPolicy.apply(activity.window)
+            }
+
+            override fun onActivityStarted(activity: Activity) = Unit
+            override fun onActivityResumed(activity: Activity) = Unit
+            override fun onActivityPaused(activity: Activity) = Unit
+            override fun onActivityStopped(activity: Activity) = Unit
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+            override fun onActivityDestroyed(activity: Activity) = Unit
+        })
         SyncWorkScheduler.schedule(this)
     }
 }
